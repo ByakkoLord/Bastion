8c912e4eace0705c0c86fb097a8a3bb090e66806
ByakkoLord <g.fonsecadacosta@gmail.com>
2024-04-07 18:25:47 -0300

    add main.tsx

--- bastion-electron-app/src/renderer/src/App.tsx
+++ bastion-electron-app/src/renderer/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Main from "./components/Main"
 
 function App(): JSX.Element {
   return (
     <body id="body">
       <Header />
+      <Main />
     </body>
   )
 }
