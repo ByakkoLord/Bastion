fa3eda4beb9a5322855f7b83b9ef8f97afd6f18f
ByakkoLord <g.fonsecadacosta@gmail.com>
2024-04-08 23:49:05 -0300

    new component

---
+++ bastion-electron-app/src/renderer/src/components/Group.tsx
@@ -0,0 +1,11 @@
+import Note from './Note';
+
+export default function Group(): JSX.Element {
+    return (
+        <section id="group">
+            <h2 id="group-name">Empresa</h2>
+            <Note />
+            <Note />
+        </section>
+    )
+}
